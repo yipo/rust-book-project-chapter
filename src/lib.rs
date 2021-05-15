9e69f39f17defb92d3edc48bd4bcfe44e1239426
yipo <yipo.tw@hotmail.com>
2021-05-15 17:54:56 +0800

    Remove the 'println!' statements
    ... that we used to check the program's behavior.

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,5 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
-
     Ok(())
 }
