101deabea288a564b09666338097a0f558980c97
yipo <yipo.tw@hotmail.com>
2021-05-15 16:12:39 +0800

    Handling Errors Returned from 'run' in 'main'

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
