0f9ccd1014ceea17316d90d3208b4bb581159063
yipo <yipo.tw@hotmail.com>
2021-05-15 16:22:17 +0800

    Splitting Code into a Library Crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use std::error::Error;
 use std::fs;
 
-struct Config {
-    query: String,
-    filename: String,
+pub struct Config {
+    pub query: String,
+    pub filename: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -19,7 +19,7 @@ impl Config {
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::process;
 
+use minigrep::Config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -12,7 +14,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
