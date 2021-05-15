bf4cd4f09ec53c0ad402ecdf683f1096269fa260
yipo <yipo.tw@hotmail.com>
2021-05-15 22:02:25 +0800

    Iterating Through Lines with the 'lines' Method

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    for line in contents.lines() {
+        // do somthing with line
+    }
 }
 
 #[cfg(test)]
