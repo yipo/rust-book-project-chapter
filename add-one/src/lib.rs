d6e39500089c4f316aa8883c4fc961a54777425f
yipo <yipo.tw@hotmail.com>
2021-07-18 21:55:53 +0800

    Adding a Test to a Workspace

--- add-one/src/lib.rs
+++ add-one/src/lib.rs
@@ -6,8 +6,10 @@ pub fn add_one(x: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(3, add_one(2));
     }
 }
